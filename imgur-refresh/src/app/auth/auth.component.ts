c9973797d9e59742d332e3065ddae7d9d19ef0d7
Walter Sandmeier <wms0011@mix.wvu.edu>
2021-11-09 14:14:56 -0500

    Authentication Messages on Screen

--- imgur-refresh/src/app/auth/auth.component.ts
+++ imgur-refresh/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 import { AuthResponse } from "./authResponse";
@@ -11,8 +12,9 @@ import { AuthResponse } from "./authResponse";
 export class AuthComponent{
     public buttonClicked!:string;
     private authObservable!:Observable<AuthResponse>;
+    public success:string = "";
     constructor(private authService:AuthService){
-
+        
     }
     public onSubmit(data:NgForm){
         console.log("Auth Button Clicked: "+ this.buttonClicked)
@@ -21,10 +23,11 @@ export class AuthComponent{
         if (this.buttonClicked == 'SignUp'){
             this.authService.signUp(data.value.email, data.value.password).subscribe(
                 (data:AuthResponse) =>{
-                    console.log(data);
-                    alert("Signed Up")
+                    console.log("Congratulations on Signing UP");
+                    this.success = ("Congratulations on Signing Up")
                 },
                 error =>{
+                    console.log("Sign up failed, reason: " + error.error.error.message)
                     alert("Sign up failed, reason: " + error.error.error.message);
                 }
             );
@@ -32,10 +35,11 @@ export class AuthComponent{
         if (this.buttonClicked == 'SignIn'){
             this.authService.signIn(data.value.email, data.value.password).subscribe(
                 (data:AuthResponse) =>{
-                    console.log(data);
-                    alert("Logged In")
+                    console.log("You are now signed in");
+                    this.success = ("You are now signed in")
                 },
                 error =>{
+                    console.log("Sign in failed, reason: " + error.error.error.message)
                     alert("Sign in failed, reason: " + error.error.error.message);
                 }
             );
